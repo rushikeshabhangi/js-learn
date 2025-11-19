bda566d33b4837e34a8d3dcc8e974a89115613e9
rushikeshabhangi <raviabhangi@gmail.com>
2025-11-19 11:53:43 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are useing nodejs not browser
+
+console.log("5 + 5"); 
+
+console.log("Rushikesh");
+
+let name = "Rushikesh"
+let age = 30
+let isLoggedIn = false
+let state;
+
+// number => 2 to pwer 53
+// bigint
+// string => ""
+// boolean => true / false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object =>
+
+console.log(typeof age);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
