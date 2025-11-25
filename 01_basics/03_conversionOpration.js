0d39f817b36d931cbfcc83d04fdccf41fa5e24cc
rushikeshabhangi <raviabhangi@gmail.com>
2025-11-25 10:29:11 +0000

    Conversion

---
+++ 01_basics/03_conversionOpration.js
@@ -0,0 +1,35 @@
+let score = "33asfdadf"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+
+
+
+let isLoggedIn = "Rushi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Rushi" => true
+
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
