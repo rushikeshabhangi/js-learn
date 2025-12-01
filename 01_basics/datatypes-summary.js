1da9e1ab95a9c88578ae5e3290f881ddc95a6070
rushikeshabhangi <raviabhangi@gmail.com>
2025-12-01 10:47:33 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,26 @@
+// Primitive Data Types
+    // 7 Types: String, Number, Boolean, null, undefined, symbol, BigInt
+    
+const stringType = "Rushikesh"
+const numberType = 1234567890
+const booleanType = false
+const nullType = null
+let undefinedType
+const symboleId = Symbol('456')
+const symboleIdSame = Symbol('456')
+const bigIntType = 123456789321654987n
+
+console.log(typeof stringType)
+console.log(typeof numberType)
+console.log(typeof booleanType)
+console.log(typeof nullType)
+console.log(typeof undefinedType)
+
+console.log(symboleId === symboleIdSame)
+console.log(typeof bigIntType)
+
+
+
+
+// Non Primitive Data Types
+    // 3 Types: Array, Objects, Functions
