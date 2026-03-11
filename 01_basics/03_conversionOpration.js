3959a6ffdb0d31f45585050f675aed1e8f52738a
rushikeshabhangi <raviabhangi@gmail.com>
2026-03-11 13:31:29 +0000

    added Oprations

--- 01_basics/03_conversionOpration.js
+++ 01_basics/03_conversionOpration.js
@@ -33,3 +33,55 @@ let stringNumber = String(someNumber)
 
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+
+/* ============================= Porations ================================== */
+let value = 3
+let nagValue = -value
+let nagValue2 = value-
+console.log(nagValue); // -3
+console.log(nagValue2); // NaN
+
+console.log(2+2);
+console.log(2-2);
+console.log(2**2); // 2x2 = 4
+console.log(2**5); // 2x2x2x2x2 = 32
+
+let fName = "Rushi"
+let lName = " Abhangi"
+let fullName = fName + lName
+console.log(fullName); // Rushi Abhangi
+console.log(fName + lName); // Rushi Abhangi
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("2" + "2"); // 22
+console.log(1 + 2 + "2"); // 32
+console.log("2" + 1 + 2); // 212
+
+console.log(2 + 2 - 2); // 2
+console.log(2 + 2 * 2); // 6
+console.log((2 + 2) * 2); // 8
+
+console.log(true); // true
+console.log(+true); // 1
+console.log(-true); // -1
+console.log(-""); // -0
+console.log(+""); // 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+console.log(num1);
+
+let counter = 100
+//++counter // 101
+counter++ 
+console.log(counter); // 101
+
+
+
+
+
+
+
+
