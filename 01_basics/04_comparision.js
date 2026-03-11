e8d69948f8de3d8d66d51b1d6ef0875a78a29fe5
rushikeshabhangi <raviabhangi@gmail.com>
2026-03-11 13:45:25 +0000

    added === log

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -17,3 +17,8 @@
 // console.log(undefined > 0); // false
 // console.log(undefined = 0); // 0
 // console.log(undefined >= 0); // false
+
+// ===
+console.log("2" == 2); // true
+console.log("2" === 2); // false
+
