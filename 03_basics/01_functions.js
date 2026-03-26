094008729b859907e1f9d7556c0a8e96d1e70689
rushikeshabhangi <raviabhangi@gmail.com>
2026-03-26 12:18:38 +0000

    Function js

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,22 @@
+
+
+function myName(){
+    console.log("R");
+    console.log("U");
+    console.log("S");
+    console.log("H");
+    console.log("I");
+}
+
+//myName();
+    // Parameters: "number1"
+function addTwoNumbers(number1, number2, number3){
+    console.log(number1 + number2 + number3);
+    
+}
+
+// addTwoNumbers(2, 3 ,"R");
+    // Function Refrence: "addTwoNumbers", Execute: ()
+    // Argument: " 2, 3 ,"R" "
+const myfunction = addTwoNumbers(2, 3 ,"R");
+console.log(myfunction);
