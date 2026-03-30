7ee2be39a334bdb54d32ec2f3707d253d6fc9259
rushikeshabhangi <raviabhangi@gmail.com>
2026-03-30 12:21:48 +0000

    scops update

--- 03_basics/02_scops.js
+++ 03_basics/02_scops.js
@@ -5,11 +5,40 @@ if (true) {
     let a = 10
     const b = 20
     var c = 30
-    console.log("Inner: ", a);
+    //console.log("Inner: ", a);
     
 }
 
-console.log(a);
+//console.log(a);
 //console.log(b);
 //console.log(c);
+
+function one(){
+    const other = "Rushi"
+
+    function two(){
+        const web = "youtube"
+        //console.log(other);
+    }
+
+    //console.log(web);
+
+    two()
+}
+one()
+
+if (true){
+    const user = "Rushikesh"
+    if (user === "Rushikesh"){
+        const web = " Google"
+        //console.log(user + web);
+    }
+    //console.log(web);
+    
+}
+
+//console.log(user);
+
+
+// ================ Intresting ==================== //
 
